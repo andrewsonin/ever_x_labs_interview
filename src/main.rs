feb8d1b93fe2abadd961d304bf54e9b1e0edbf5e
Andrew Sonin <sonin.cel@yandex.ru>
2022-06-02 05:04:51 +0300

    Final solution

--- src/main.rs
+++ src/main.rs
@@ -1,91 +1,44 @@
-use std::collections::hash_map::Entry;
-use std::collections::HashMap;
-use std::iter::once;
-use std::ops::Range;
-
-#[derive(Default)]
-struct RangeCounter {
-    best_start: usize,
-    best_end: usize,
-    cur_start: usize,
-    cur_end: usize,
+fn get_idx_and_max<T: Ord + Copy>(values: &[T]) -> Option<(usize, T)> {
+    values
+        .iter()
+        .enumerate()
+        .max_by_key(|(_idx, &val)| val)
+        .map(|(idx, val)| (idx, *val))
 }
 
-impl RangeCounter {
-    fn on_brace_mismatch(&mut self, next_idx: usize) {
-        self.update_best_range();
-        self.cur_start = next_idx;
-        self.cur_end = next_idx;
-    }
-
-    fn update_best_range(&mut self) {
-        if self.cur_end - self.cur_start > self.best_end - self.best_start {
-            self.best_start = self.cur_start;
-            self.best_end = self.cur_end;
-        }
-    }
-
-    fn shift_end(&mut self, next_idx: usize) {
-        self.cur_end = next_idx;
-    }
-
-    fn get_best_range(&self) -> Range<usize> {
-        self.best_start..self.best_end
-    }
-}
-
-#[derive(Eq, PartialEq)]
-enum PendingBracket {
-    Round,
-    Square,
-    Curly,
-}
-
-fn get_longest_correct_cyclic_substring(s: impl AsRef<str>) -> String
+fn get_longest_correct_cyclic_substring(s: &str) -> String
 {
-    let s = s.as_ref();
     let s_len = s.len();
     if s_len != s.chars().count() {
         panic!("Input string should contain 1-byte symbols only. Got: {s}")
     }
-    let triple_s = format!("{s}{s}{s}");
+    let double_s = format!("{s}{s}");
 
-    let (mut round_sum, mut square_sum, mut curly_sum) = (0, 0, 0);
-    let mut min_max_occurrences = HashMap::<(i64, i64, i64), (usize, usize)>::with_capacity(
-        s_len * 3
-    );
-    min_max_occurrences.insert((0, 0, 0), (0, 0));
-    for (char, idx) in triple_s.chars().zip(1usize..) {
-        match char {
-            '(' => round_sum += 1,
-            ')' => round_sum -= 1,
-            '[' => square_sum += 1,
-            ']' => square_sum -= 1,
-            '{' => curly_sum += 1,
-            '}' => curly_sum -= 1,
-            _ => {}
-        }
-        match min_max_occurrences.entry((round_sum, square_sum, curly_sum)) {
-            Entry::Occupied(mut entry) => {
-                let (_min_idx, max_idx) = entry.get_mut();
-                *max_idx = idx
+    let mut num_correct_behind = vec![0; s_len * 2];
+    for (idx, symbol) in double_s.chars().enumerate().skip(1) {
+        if symbol.is_alphabetic() {
+            num_correct_behind[idx] = num_correct_behind[idx - 1] + 1
+        } else {
+            let comparative_symbol = match symbol {
+                '(' | '[' | '{' => continue,
+                ')' => '(',
+                ']' => '[',
+                '}' => '{',
+                _ => panic!("Does not expect such a symbol: {symbol}")
+            };
+            let prev_idx = idx - 1;
+            let idx_to_compare = prev_idx - num_correct_behind[prev_idx];
+            if double_s.as_bytes()[idx_to_compare] as char == comparative_symbol {
+                num_correct_behind[idx] = 2 + num_correct_behind[prev_idx] + num_correct_behind[idx_to_compare - 1];
             }
-            Entry::Vacant(entry) => { entry.insert((idx, idx)); }
         }
     }
-    let (best_dist, best_left, best_right) = min_max_occurrences.values().fold(
-        (0, 0, 0),
-        |(mut best_dist, mut best_left, mut best_right), &(min_idx, max_idx)| {
-            let cur_dist = max_idx - min_idx;
-            if cur_dist > best_dist {
-                (best_dist, best_left, best_right) = (cur_dist, min_idx, max_idx)
-            }
-            (best_dist, best_left, best_right)
-        },
-    );
 
-    let result = if best_dist < s_len {
-        &triple_s[best_left..best_right]
+    let (idx, max) = get_idx_and_max(&num_correct_behind).unwrap_or_else(|| (0, 0));
+
+    let best_substring_range = (idx + 1 - max)..(idx + 1);
+    let result = if best_substring_range.len() < s_len {
+        &double_s[best_substring_range]
     } else {
         "Infinite"
     };
@@ -140,10 +93,16 @@ mod tests {
     fn example_09() {
         assert_eq!(get_longest_correct_cyclic_substring("[dh()wed(())[]"), "dh()wed(())[]")
     }
+
+    #[test]
+    fn example_10() {
+        assert_eq!(get_longest_correct_cyclic_substring("[dh()wEd(([))[]"), "dh()wEd")
+    }
+
+    #[test]
+    fn example_11() {
+        assert_eq!(get_longest_correct_cyclic_substring("a"), "Infinite")
+    }
 }
 
-fn main() {
-    println!("{}", get_longest_correct_cyclic_substring("}](){"));
-    println!("{}", get_longest_correct_cyclic_substring("sh(dh)}"));
-    println!("{}", get_longest_correct_cyclic_substring("]h({hdb}b)["));
-}
+fn main() {}
